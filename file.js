93701ef6289223d4b3592b769958b7fde8be3856
Your Name <arpansaha0707@gmail.com>
2026-08-26 12:38:19 +0530

    Initial Node.js setup

---
+++ file.js
@@ -0,0 +1,6 @@
+console.log("Hello, Node.js!");
+
+// console.log(window); // This will throw an error because 'window' is not defined in Node.js
+
+
+console.log(globalThis); // This will log the global object in Node.js
