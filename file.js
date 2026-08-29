821504d75a1fce9027f0b908f024dd998bd33898
Your Name <arpansaha0707@gmail.com>
2026-08-29 11:50:10 +0530

    module pratice today

--- file.js
+++ file.js
@@ -3,4 +3,11 @@ console.log("Hello, Node.js!");
 // console.log(window); // This will throw an error because 'window' is not defined in Node.js
 
 
-console.log(globalThis); // This will log the global object in Node.js
+// console.log(globalThis); // This will log the global object in Node.js
+function add ( a, b) {
+    return a + b;
+}
+
+// module.exports = "Arpan";
+
+module.exports = add; // Exporting the add function so that it can be used in other files
---
+++ module.js
@@ -0,0 +1,6 @@
+"use strict";
+
+
+const math = require("./file.js"); // must have to use ./ it means current directory .
+
+console.log(math(2, 3)); // Output: 5
