7ee799f2cba411324b755c755d305c1e55bff2d5
Your Name <arpansaha0707@gmail.com>
2026-08-29 16:32:35 +0530

    module learnign

--- file.js
+++ file.js
@@ -2,12 +2,23 @@ console.log("Hello, Node.js!");
 
 // console.log(window); // This will throw an error because 'window' is not defined in Node.js
 
-
 // console.log(globalThis); // This will log the global object in Node.js
-function add ( a, b) {
-    return a + b;
-}
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function subtract(a, b) {
+//   return a - b;
+// }
 
 // module.exports = "Arpan";
 
-module.exports = add; // Exporting the add function so that it can be used in other files
+// module.exports = {
+//     add : add,
+//     sub : subtract
+// } // Exporting the add function so that it can be used in other files
+
+
+exports.add = (a,b = 7) => {
+    console.log(a + b)
+}
--- module.js
+++ module.js
@@ -1,6 +1,9 @@
 "use strict";
 
 
-const math = require("./file.js"); // must have to use ./ it means current directory .
+const {add,sub} = require("./file.js"); // must have to use ./ it means current directory .
+// I must have to destructure to get multiple functions value .
 
-console.log(math(2, 3)); // Output: 5
+// console.log(add(2, 3)); // Output: 5
+// console.log(sub(4,2))
+add(7)
