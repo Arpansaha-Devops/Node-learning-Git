62395976e46edf2c71bb387045e0772b3e965c29
Your Name <arpansaha0707@gmail.com>
2026-09-03 16:48:57 +0530

    OS module learn

--- Blocking Vs NonBlocking/Test.js
+++ Blocking Vs NonBlocking/Test.js
@@ -8,6 +8,15 @@ const fs = require("fs");
 // });
 
 
+const os = require("os");
+
+console.log("Platform:", os.platform());
+console.log("Architecture:", os.arch());
+console.log("CPU:", os.cpus().length);
+console.log("Total memory:", os.totalmem());
+// console.log("Free memory:", os.freemem());
+// console.log("Hostname:", os.hostname());
+// console.log("Home directory:", os.homedir());
 
 
 // console.log("1");
@@ -27,7 +36,7 @@ const fs = require("fs");
 
 // This is a Non-Blocking Code ----- because it will not block the execution until the file is written to the disk .
 
-console.log("1");
+// console.log("1");
 
  fs.readFile("./Text.txt", "utf-8", (err, data) => {
  console.log(data);
@@ -35,4 +44,4 @@ console.log("1");
 
 
 
-console.log("2");
+// console.log("2");
