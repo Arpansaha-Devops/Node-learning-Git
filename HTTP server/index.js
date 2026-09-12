11c4d6eae255e6594311912f8a8e7a8b835fe380
Your Name <arpansaha0707@gmail.com>
2026-09-12 12:25:25 +0530

    URL Handling

--- HTTP server/index.js
+++ HTTP server/index.js
@@ -1,11 +1,15 @@
 const http = require("http") ; 
 const fs = require("fs") ;
+const url = require("url") ;
 
 
 const myServer = http.createServer((req,res) => {
+   if(req.url === "/favicon.ico") return res.writeHead(204); // To avoid favicon.ico error in console log .
    const log = `${Date.now()} : New Request Made : ${req.url} : ${req.method}\n`;
+   const myUrl = url.parse(req.url,true);
+   console.log(myUrl)
   fs.appendFile("log.txt", log , (err) => {
-     switch(req.url){
+     switch(myUrl.pathname){
       case "/":
         res.writeHead(200, {"Content-Type" : "text/html"});
         res.end("<h1>Welcome to Home Page</h1>");
@@ -14,9 +18,15 @@ const myServer = http.createServer((req,res) => {
         res.writeHead(200, {"Content-Type" : "text/html"});
         res.end("<h1>Welcome to About Page</h1>");
         break;
-        case "/contact":
+        case  "/search" :
+        const searchUrl = myUrl.query.search_query;
         res.writeHead(200, {"Content-Type" : "text/html"});
-        res.end("<h1>Welcome to Contact Page</h1>");
+        res.end(`<h1>Welcome to Search Page</h1><p>You searched for: ${searchUrl}</p>`);
+        break;
+        case "/contact":
+         const userName = myUrl.query.name;
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.end(`<h1>Welcome to Contact Page</h1><p>Hello, ${userName}!</p>`);
         break;
         default:
         res.writeHead(404, {"Content-Type" : "text/html"});
