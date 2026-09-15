aa0a1f31b0ff5cc6367dc40a8024db27ea38da8b
Your Name <arpansaha0707@gmail.com>
2026-09-15 16:55:53 +0530

    Creating Rest_API

---
+++ Rest_API/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();  // it creates an instance of express application
+const port = 3000; // port number on which the server will listen
+const users = require("./MOCK_DATA.json"); // importing the JSON data from the file
+
