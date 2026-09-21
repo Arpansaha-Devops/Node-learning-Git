7931b8b63875decc2922d1e778f6ae726c88dc9a
Arpan <arpansaha0707@gmail.com>
2026-09-21 23:08:04 +0530

    using middleware for log's

--- midddleWare Basics/index.js
+++ midddleWare Basics/index.js
@@ -2,6 +2,7 @@ const express = require('express'); //
 const app = express(); // creating an instance of the Express application
 const PORT = 3000; // defining the port number on which the server will listen
 const users = require('./MOCK_DATA.json'); // importing the JSON data from the file
+const fs = require('fs'); // importing the file system module to read and write files
 
 app.use(express.json()); // middleware to parse incoming JSON requests
 
@@ -9,6 +10,11 @@ app.use(express.json()); // middleware to parse incoming JSON requests
 
 app.use((req, res, next) => {
   console.log('Request received:', req.method, req.url);
+  fs.appendFile('request_logs.txt', `${new Date().toISOString()} - ${req.method} ${req.url}\n`, (err) => {
+    if (err) {
+      console.error('Error writing to log file:', err);
+    }
+});
    next(); // calling next() to pass control to the next middleware function.
 // if next() is not called, the request will be left hanging and the client will not receive a response.
 });
